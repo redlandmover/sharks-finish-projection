1a70554603adc766a0b1a13072bd5b2a6454a10d
Matt English <redlandmover@gmail.com>
2015-03-18 11:11:57 -0700

    added ROW and mcdavid %

--- test.rb
+++ test.rb
@@ -18,6 +18,7 @@ teams = rows.collect do |row|
     [:name, 'td[1]/a/text()'],
     [:games_played, 'td[2]/text()'],
     [:points, 'td[6]/text()'],
+    [:row, 'td[7]/text()']
   ].each do |name, xpath|
     detail[name] = row.at_xpath(xpath).to_s.strip
   end
@@ -25,9 +26,12 @@ teams = rows.collect do |row|
 end
 #ap teams
 
+percentages = [20, 13.5, 11.5, 9.5, 8.5,7.5,6.5,6,5,3.5,3,2.5,2,1]
+
 teams.each{ |team|
   team[:games_played] = team[:games_played].to_i
   team[:points] = team[:points].to_i
+  team[:row] = team[:row].to_i
   games_left = {:games_left => 82-team[:games_played] }
   team.merge!(games_left)
   max_points = {:max_points => team[:games_left]*2 + team[:points]}
@@ -37,6 +41,15 @@ teams.each{ |team|
 
 sharks = teams.find {|team| team[:name] == "San Jose"}
 
-worse_teams = teams.select {|team| team[:max_points] <= sharks[:points]}
+worst_teams = teams.select {|team| team[:max_points] <= sharks[:points]}
 
-puts "The sharks can finish a maximum of " + (worse_teams.count+1).to_s
+ap worst_teams
+
+if worst_teams[0][:maxPoints] == sharks[:points] && worst_teams[0][:row] < sharks[:row]
+  worst_teams.delete_at(0)
+end
+
+ap worst_teams
+
+puts "The sharks can finish a maximum of " + (worst_teams.count+1).to_s +
+  " and have a " + percentages[(worst_teams.count)].to_s + ' % chance at McDavid'
